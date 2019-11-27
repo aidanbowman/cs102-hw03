100336cb02023495aa16f302c2d2a23eb015c5c9
Aidan Bowman <aidan.bowman@cooper.edu>
2019-11-27 12:14:26 -0500

    Happy Thanksgiving!

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import sys
+from statistics import mean
 
 
 def main():
     assert len(sys.argv) > 1, "No input file path specified."
-
     input_file_path = sys.argv[1]
     print(f"Processing input file: {input_file_path}")
 
-    # TODO: Fill in the actual logic here!
+    with open(input_file_path, "r") as f:
+        for inputs in f:
+            inputs = inputs.strip("\n")
+            inputs_ting = inputs.split(",")
+            giraffe = []
+            for x in inputs_ting:
+                giraffe.append(float(x))
+                zebra = mean(giraffe)
+            print(f"{zebra}")
 
 
 if __name__ == "__main__":
